f91f319937ff55ad9e6a7dc4acf481e04c2c4e56
Alexandr <apexof@gmail.com>
2022-10-23 11:27:40 +0300

    asd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,37 @@
-import type { NextPage } from 'next';
-import Link from 'next/link';
-import React from 'react';
+import { NextPage } from 'next';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { WordsList } from '../components/WordsList/WordsList';
+import { AppState, useAppDispatch } from '../state';
+import { addWords } from '../state/words/words.reducer';
+import { TWordsList } from '../types';
+import s from './index.module.scss';
 
-const Home: NextPage = () => {
+const WordsPage: NextPage = () => {
+  const dispatch = useAppDispatch();
+  const { allWords, unlearnedWords } = useSelector((state: AppState) => state.words);
+
+  useEffect(() => {
+    fetch('data/words.json')
+      .then(res => res.json())
+      .then((res: TWordsList) => {
+        dispatch(addWords({ items: res }));
+      })
+      .catch(console.error);
+  }, []);
+
   return (
-    <div>
-      <Link href="words">words</Link>
+    <div className={s['words-page']}>
+      <div className={s['words-block']}>
+        <h2>All Words</h2>
+        <WordsList words={Object.values(allWords)} />
+      </div>
+      <div className={s['words-block']}>
+        <h2>Unlearned Words</h2>
+        <WordsList words={Object.values(unlearnedWords)} />
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default WordsPage;

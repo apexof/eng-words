8d391a644f0cb37be19be287a5ad6a95d9acf531
Alexandr <apexof@gmail.com>
2022-12-18 10:35:54 +0300

    sdf

--- components/WordsList/AllCategoryList.tsx
+++ components/WordsList/AllCategoryList.tsx
@@ -12,14 +12,17 @@ interface Props {
 export const AllCategoryList: FC<Props> = props => {
   const { words } = props;
   const allCategories = useSelector(selectAllCategories);
-
+  const wordsWithoutCategory = words.filter(word => !word.tags.length);
   return (
     <div className={s['all-category-list']}>
-      {allCategories.map(categoryName => {
+      {allCategories.reduce((acc: JSX.Element[], categoryName) => {
         const wordOfCurrentCat = words.filter(word => word.tags.includes(categoryName));
-
-        return <WordsListByCategory key={categoryName} words={wordOfCurrentCat} categoryName={categoryName} />;
-      })}
+        if (wordOfCurrentCat.length) {
+          acc.push(<WordsListByCategory key={categoryName} words={wordOfCurrentCat} categoryName={categoryName} />);
+        }
+        return acc;
+      }, [])}
+      <WordsListByCategory words={wordsWithoutCategory} categoryName="without category" />
     </div>
   );
 };
